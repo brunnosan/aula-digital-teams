57b845de91a74012cabdc115eeca29becc521dae
brunnosan <brunnosan@yahoo.com.br>
2024-04-20 17:03:50 -0300

    Adicionando hook form

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -4,6 +4,10 @@ import { Sidebar } from 'primereact/sidebar';
 import { Dialog } from 'primereact/dialog';
 import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
 import { useEffect, useState } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { InputMask } from 'primereact/inputmask';
+import { useForm } from 'react-hook-form';
+        
 
 const List = () => {
 
@@ -12,35 +16,92 @@ const List = () => {
   const [mostrarDialog, setMostrarDialog] = useState(false);
   const [teams, setTeams] = useState([]);
 
-  const titulo = (
-    <div className='flex justify-content-between align-items-center text-lg'>
-      título do card
-      <i className='pi pi-eye cursor-pointer' onClick={() => setMostrarDialog(true)}></i>
-    </div>
-  )
+  //Forma de usar o react-hook-form
+  //Para evitar que se crie um useRef para cada campo de um formulãrio (no caso, do formulário do sidebar)
+  //Os hooks interferem no ciclo de vida do componente (exceto o useRef)
+  //Os hooks normalmente usam "use" no começo de uma função para mostrar que indica um hook
+  //As funções "normais" são diferentes do hook por também não interferir no ciclo de vida do componente
 
-  const footer = (
-    <div className='flex gap-3'>
-      <Button label='Adicionar' className='flex-1 px-0' onClick={() => setMostrarSidebarAdd(true)} />
-      <Button icon='pi pi-trash' onClick={confirmacao}/>
-      {/* Se a função receber parâmetro, precisa abrir uma arrow function, caso contrário, pode só nomear a função */}
-    </div>
-  )
+  //Para evitar criar uma variável que tenha o objeto do hook-form, a sugestão é já criar o objeto desestruturado
+  //  caso contrário, seria usado variavel.register e se escreveria mais
+  const { register, handleSubmit, reset } = useForm();
+  const { register: registerP, handleSubmit: handleSubmitP, reset: resetP, setValue: setValueP } = useForm();
 
-  function confirmacao() {
+  async function cadastrar(dados) {
+    const request = await fetch('http://localhost:3000/teams', {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(dados)
+    })
+    const response = await request.json();
+    // console.log(response);
+    // setTeams(response);
+    if (response) {
+      reset();
+      buscarTeams();
+      setMostrarSidebar(false);
+    }
+  }
+
+  async function cadastrarP(dados) {
+    // const request = await fetch('http://localhost:3000/teams', {
+    //   method: 'post',
+    //   headers: {
+    //     'Content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(dados)
+    // })
+    // const response = await request.json();
+    // if (response) {
+    //   resetP();
+    //   setMostrarSidebarAdd(false);
+    // }
+    console.log(dados);
+    setMostrarSidebarAdd(false);
+  }
+
+  function confirmacao(id) {
     confirmDialog ({
       header: 'Aviso',
       message: 'Deseja realmente apagar este item?',
       acceptLabel: 'Sim',
       rejectLabel: 'Não',
-      accept: () => {alert('confirmado')},
+      accept: async () => {
+        await fetch(`http://localhost:3000/teams/${id}`, {
+          method: 'delete'
+        }).then(() => {  //finally do fetch é semelhante ao finally do try..catch, sempre é executado no final
+          buscarTeams();
+        })
+      },
       reject: () => {alert('cancelado')}
     })
   }
 
+  const titulo = (nome) => (
+    <div className='flex justify-content-between align-items-center text-lg'>
+      {nome}
+      <i className='pi pi-eye cursor-pointer' onClick={() => setMostrarDialog(true)}></i>
+    </div>
+  )
+
+  //Para que o botão delete receba o id como parâmetro, transforme o footer num arrow function
+  const footer = (id) => (
+    <div className='flex gap-3'>
+      <Button label='Adicionar' className='flex-1 px-0' onClick={() => {
+          setValueP('id', id);
+          setMostrarSidebarAdd(true);
+        }}
+      />
+      <Button icon='pi pi-trash' onClick={() => confirmacao(id)}/>
+      {/* Se a função receber parâmetro, precisa abrir uma arrow function, caso contrário, pode só nomear a função */}
+    </div>
+  )
+
   async function buscarTeams() {
     const request = await fetch("http://localhost:3000/teams");
-    const response = await request.json;
+    const response = await request.json();
     setTeams(response);
   }
 
@@ -60,40 +121,66 @@ const List = () => {
       {/* por conta do gap-3 que equivale a 16px, entre cada card tem 16px de espaçamento
           como são 5 cards desejados, tem 16 x 4 = 64 px
           pro calc, coloque 64 / 5 e arredonde pra cima pra descontar dos 20% (que 100% / 5 cards)
-       */}
-      <Card style={{width: 'calc(20% - 13px)'}} title={titulo} footer={footer}>
-        <h1 className='mx-auto flex flex-column text-center'>0 <span className='text-sm'>/ 0</span></h1>
-      </Card>
-      <Card style={{width: 'calc(20% - 13px)'}} title={titulo} footer={footer}>
-        <h1 className='mx-auto flex flex-column text-center'>0 <span className='text-sm'>/ 0</span></h1>
-      </Card>
-      <Card style={{width: 'calc(20% - 13px)'}} title={titulo} footer={footer}>
-        <h1 className='mx-auto flex flex-column text-center'>0 <span className='text-sm'>/ 0</span></h1>
-      </Card>
-      <Card style={{width: 'calc(20% - 13px)'}} title={titulo} footer={footer}>
-        <h1 className='mx-auto flex flex-column text-center'>0 <span className='text-sm'>/ 0</span></h1>
-      </Card>
-      <Card style={{width: 'calc(20% - 13px)'}} title={titulo} footer={footer}>
-        <h1 className='mx-auto flex flex-column text-center'>0 <span className='text-sm'>/ 0</span></h1>
-      </Card>
-      <Card style={{width: 'calc(20% - 13px)'}} title={titulo} footer={footer}>
-        <h1 className='mx-auto flex flex-column text-center'>0 <span className='text-sm'>/ 0</span></h1>
-      </Card>
+       */
+      }
+      {
+        teams && teams.map((team) => (
+            <Card key={`team${team.id}`} style={{width: 'calc(20% - 13px)'}} title={() => titulo(team.nome)} footer={() => footer(team.id)}>
+              <h1 className='mx-auto flex flex-column text-center'>
+                {team.participante.length} <span className='text-sm'>/ {team.capacidade}</span>
+              </h1>
+            </Card>
+          ))
+      }
 
       <Sidebar
         visible={mostrarSidebar}
         onHide={() => setMostrarSidebar(false)}
         position='right'
       >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur obcaecati labore, voluptas saepe corrupti magnam temporibus hic eos iure officia adipisci velit ut molestiae eligendi voluptates perspiciatis vero esse alias?
+        <form onSubmit={handleSubmit(cadastrar)}>
+          <h3>Cadastrar</h3>
+          <label htmlFor="nome" className='uppercase text-sm font-bold mb-2 block'>Nome</label>
+          <InputText
+            id="nome"
+            placeholder='Digite o nome do time'
+            className='w-full md-3'
+            {...register('nome', {required: true})}
+          />
+          <label htmlFor="capacidade" className='uppercase text-sm font-bold mb-2 block'>Capacidade</label>
+          <InputMask
+            id="capacidade"
+            mask={'99'}
+            className="w-full mb-3"
+            {...register('capacidade', {required: true})}
+          />
+          <Button
+            label="Criar"
+            className="w-full"
+          />
+        </form>
       </Sidebar>
 
       <Sidebar
         visible={mostrarSidebarAdd}
         onHide={() => setMostrarSidebarAdd(false)}
-        position='rigt'
+        position='right'
       >
-        Sidebar Botão Add
+        <form onSubmit={handleSubmitP(cadastrarP)}>
+          <h3>Adicionar</h3>
+          <input type="hidden" {...registerP('id')} />
+          <label htmlFor="nome" className='uppercase text-sm font-bold mb-2 block'>Nome</label>
+          <InputText
+            id="nome"
+            placeholder='Digite o nome do participante'
+            className='w-full md-3'
+            {...registerP('nome', {required: true})}
+          />
+          <Button
+            label="Criar"
+            className="w-full"
+          />
+        </form>
       </Sidebar>
 
       <Dialog
